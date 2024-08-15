8c3b3f750b85cc8d4e5f978db8927d93bd697d71
aditya choubisa <adityachoubisa02@gmail.com>
2024-08-15 10:51:46 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by APPLE on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
